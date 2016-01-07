ddb9bf7ed5b358e9e0edcb94daeac7cde184e297
xyxiao <463618622@qq.com>
2016-01-07 11:32:53 +0800

    change blog

--- js/blog.js
+++ js/blog.js
@@ -1,25 +1,24 @@
-var app = {};
-app.menu = function () {
-  $('#menu-bar').click(function (event) {
-    event.preventDefault();
-    if ($(this).hasClass('fa-bars')) {
-      $(this).removeClass('fa-bars')
-        .addClass('fa-times');
-      $('#menu-ul').removeClass('collapse');
+
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
-      $(this).removeClass('fa-times')
-        .addClass('fa-bars');
-      $('#menu-ul').addClass('collapse');
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-  });
-  $('.bg').click(function () {
-    if ($('#menu-bar').hasClass('fa-times')){
-      $('#menu-bar').removeClass('fa-times')
-        .addClass('fa-bars');
-      $('#menu-ul').addClass('collapse');
-    }
-  });
-};
-$(function () {
-  app.menu();
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
 });
